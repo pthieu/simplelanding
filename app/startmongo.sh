3b2ee021a6e9d91019f2b6de0e891f2d42ba962b
Phong <pthieu@gmail.com>
2014-10-14 00:15:52 -0400

    changing mongod script to use thing

--- app/startmongo.sh
+++ app/startmongo.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #start mongod service
-/usr/bin/mongod --config ~/www/EA/mongodb.config --dbpath ~/www/db/ --fork --logpath ~/www/EA/mongod.log
+/usr/bin/mongod --config ~/www/EA/mongodb.config --dbpath ~/www/db/ --fork --logpath ~/www/EA/log/mongod.log
